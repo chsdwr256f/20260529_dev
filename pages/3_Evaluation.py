c15e00e1b9d2cb4d1a959db3172c4a294b3bb3ae
chsdwr256f <160517760+chsdwr256f@users.noreply.github.com>
2026-07-12 18:34:04 +0100

    Update 3_Evaluation.py

--- pages/3_Evaluation.py
+++ pages/3_Evaluation.py
@@ -6,7 +6,7 @@ import streamlit as st
 import gspread
 
 from google.oauth2.service_account import Credentials
-
+from utils import show_consent_dialog
 
 st.set_page_config(
     page_title="Evaluation",
